1736ed8694e94f8ed5f608bafebd7cb79d91e875
Dmitry <dm1.f3d@gmail.com>
2017-12-07 23:14:50 +0300

    test

--- app/src/main/java/com/projectcenterfvt/historicalpenza/MainActivity.java
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+//санина
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
