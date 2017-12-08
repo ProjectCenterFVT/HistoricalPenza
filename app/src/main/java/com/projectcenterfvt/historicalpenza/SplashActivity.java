da7ce893a9ffee5b4218695c8260b370eefa5cda
Dmitry <dm1.f3d@gmail.com>
2017-12-08 22:43:24 +0300

    Add splash screen with layout

---
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.projectcenterfvt.historicalpenza;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
