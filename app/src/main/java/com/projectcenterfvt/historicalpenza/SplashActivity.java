0eb50ba8e632444d3a5ffd36af510be16a6eca53
Dmitry <dm1.f3d@gmail.com>
2017-12-10 17:01:07 +0300

    Add greeting activity

--- app/src/main/java/com/projectcenterfvt/historicalpenza/SplashActivity.java
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.projectcenterfvt.historicalpenza;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 2000;
 
+    static final String KEY_IS_FIRST_TIME = "first_time";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,21 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent;
+
+                if(isFirstTime()) {
+                    getPreferences(Context.MODE_PRIVATE).edit().putBoolean(KEY_IS_FIRST_TIME, false).apply();
+                    intent = new Intent(SplashActivity.this, GreetingActivity.class);
+                } else
+                    intent = new Intent(SplashActivity.this, ActivityMap.class);
+
                 SplashActivity.this.startActivity(intent);
                 SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    public boolean isFirstTime() {
+        return getPreferences(Context.MODE_PRIVATE).getBoolean(KEY_IS_FIRST_TIME, true);
     }
 }
