f39557c9c73613730e33a6d3555bfebe17c75f10
Dmitry <dm1.f3d@gmail.com>
2017-12-14 00:06:00 +0300

    Добавить ViewPager

---
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/PageFragment.java
@@ -0,0 +1,53 @@
+package com.projectcenterfvt.historicalpenza;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
+
+/**
+ * Created by Dmitry on 13.12.2017.
+ */
+
+public class PageFragment extends Fragment {
+
+    static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+
+    int pageNumber;
+    int backColor;
+
+    public static PageFragment newInstance(int page) {
+        PageFragment pageFragment = new PageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+
+        Random rnd = new Random();
+        backColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null);
+
+        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+        tvPage.setText("Page " + pageNumber);
+        tvPage.setBackgroundColor(backColor);
+
+        return view;
+    }
+}
