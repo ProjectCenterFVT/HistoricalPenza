abf93c1fa269391da2835c3d06ad5789245bc9ae
Dmitry <dm1.f3d@gmail.com>
2017-12-14 17:06:50 +0300

    Доработать экран информации и исправить темы

--- app/src/main/java/com/projectcenterfvt/historicalpenza/GreetingActivity.java
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/GreetingActivity.java
@@ -26,11 +26,6 @@ public class GreetingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_greeting);
 
 
--- app/src/main/java/com/projectcenterfvt/historicalpenza/info_activity.java
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/info_activity.java
@@ -1,14 +1,19 @@
 package com.projectcenterfvt.historicalpenza;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
-public class info_activity extends Activity {
+public class info_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_activity);
+
+        TextView tvDescription = (TextView) findViewById(R.id.tvDescription);
+        tvDescription.setMovementMethod(new ScrollingMovementMethod());
 
     }
 
