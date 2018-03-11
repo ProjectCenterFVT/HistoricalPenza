5e045b25b1f1db176ff4a25e3dc810893de32cc8
Suvorov <maksimsuvorovru@gmail.com>
2018-03-12 00:20:46 +0300

    Сделан Аккаунтпикер.НЕЛЬЗЯ СЛИВАТЬ. Временно убран переход на greeting и  на карту. Взаимодействий с сервером нет. Клавиша прорисовывается всегда.

--- app/src/main/java/com/projectcenterfvt/historicalpenza/Activity/SplashActivity.java
+++ app/src/main/java/com/projectcenterfvt/historicalpenza/Activity/SplashActivity.java
@@ -4,10 +4,22 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.projectcenterfvt.historicalpenza.DataBases.DB_Position;
 import com.projectcenterfvt.historicalpenza.DataBases.Sight;
 import com.projectcenterfvt.historicalpenza.R;
@@ -23,7 +35,7 @@ import com.projectcenterfvt.historicalpenza.Server.ClientServer;
  * @since 1.0.0
  */
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener{
 
     /** ID, по которому выводятся логи в Logcat*/
     static final String TAG = "server";
@@ -31,6 +43,9 @@ public class SplashActivity extends AppCompatActivity {
     static final String KEY_IS_FIRST_TIME = "first_time";
     /** Экземпляр класс базы данных*/
     private static DB_Position db;
+    private SignInButton sign_in_button;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
 
     /**
      * Метод вызывается при создании или перезапуска активности <br>
@@ -45,7 +60,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final TextView textViewHistoric;
+        final TextView textViewPenza;
+        textViewHistoric = findViewById(R.id.textViewHistorical);
+        textViewPenza = findViewById(R.id.textView8);
+        sign_in_button = (SignInButton) findViewById(R.id.sign_in_button);
+        sign_in_button.setOnClickListener(this);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions)
+                .build();
         db = new DB_Position(this);
+
 
         ClientServer call = new ClientServer(this);
         call.setOnResponseListener(new ClientServer.OnResponseListener<Sight>() {
@@ -80,17 +106,7 @@ public class SplashActivity extends AppCompatActivity {
 
                 db.close();
 
-                Intent intent;
-                if (isFirstTime()) {
-                    getPreferences(Context.MODE_PRIVATE).edit().putBoolean(KEY_IS_FIRST_TIME, false).apply();
-                    intent = new Intent(SplashActivity.this, GreetingActivity.class);
-                } else
-                    intent = new Intent(SplashActivity.this, MapActivity.class);
-
-                SplashActivity.this.startActivity(intent);
-                SplashActivity.this.finish();
             }
-
             /**
              * Метод вызывается при неудачном подключении
              */
@@ -101,6 +117,7 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent;
                 if (isFirstTime()) {
                     getPreferences(Context.MODE_PRIVATE).edit().putBoolean(KEY_IS_FIRST_TIME, false).apply();
+
                     intent = new Intent(SplashActivity.this, GreetingActivity.class);
                 } else
                     intent = new Intent(SplashActivity.this, MapActivity.class);
@@ -117,6 +134,39 @@ public class SplashActivity extends AppCompatActivity {
      * @return булевое значение
      */
     public boolean isFirstTime() {
-        return getPreferences(Context.MODE_PRIVATE).getBoolean(KEY_IS_FIRST_TIME, true);
+        //return getPreferences(Context.MODE_PRIVATE).getBoolean(KEY_IS_FIRST_TIME, true);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.sign_in_button:
+            signIn(); break;
+        }
+    }
+private void signIn(){
+Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+startActivityForResult(intent,REQ_CODE);
+}
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+    private void handleResult(GoogleSignInResult result){
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_CODE) {
+            GoogleSignInResult  result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+        }
     }
 }
